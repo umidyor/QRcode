b200e2548735a39814878580786cd492fb28132c
umidyor asomov <106219812+umidyor@users.noreply.github.com>
2022-10-08 17:47:40 +0500

    Create qrcode.py

---
+++ qrcode.py
@@ -0,0 +1,11 @@
+#pip install pyqrcode
+#pip install pypng
+
+import pyqrcode
+
+text = input("Enter your text: ")
+name = input("Enter a desired name for the QRcode>> ")
+scale = int(input("Select the size you want for the QRcode>> "))
+
+qrcode = pyqrcode.create(text, error='L')
+qrcode.png(f"{name}.png", scale=scale)
